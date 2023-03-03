abfa2050efc36807b0be23474745ee6a03f80e09
usagibryan <34449029+usagibryan@users.noreply.github.com>
2023-03-03 16:16:12 -0500

    Add more formats

--- finder.py
+++ finder.py
@@ -3,6 +3,19 @@ import re
 print('Enter a message to scan for phone numbers: ')
 message = input()
 
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+# phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+# phoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}') # shortened wtih braces
+# phoneNumRegex = re.compile(r'(\d{3})-(\d{3}-\d{4})') # grouping with parentheses
+
+# phoneNumRegex = re.compile(r'(\(\d\d\d\)) (\d\d\d-\d\d\d\d)') # area code in () and grouped
+phoneNumRegex = re.compile(r'(\(\d{3}\)) (\d{3}-\d{4})') # shortened with braces
+
 mo = phoneNumRegex.search(message)
-print('Phone number found: ' + mo.group())
+print('Phone number found: ' + mo.group()) # mo.group(0) will also return the entire matched text
+print('The area code is: ' + mo.group(1))
+print('The local exchange or subscriber number is: ' + mo.group(2))
+
+# print(mo.groups())
+# areaCode, mainNumber = mo.groups()
+# print(areaCode)
+# print(mainNumber)
